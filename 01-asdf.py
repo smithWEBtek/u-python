a66d59595c35afb76634b56befbd7544128263da
smithwebtek <brad@smithwebtek.com>
2019-02-22 10:54:11 -0500

    Strings in Python

---
+++ 01-asdf.py
@@ -0,0 +1,70 @@
+# print('hello \npython')
+# print('hello \tpython')
+# print(len('hello'))
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# slice
+# print(letters[2:5])
+# print(letters[0:3])
+# print(letters[:3])
+# print(letters[3:6])
+# print(letters[2:8])
+
+# step
+# print(nums[0:19:7])
+# print(nums[::])
+# print(nums[::5])
+# print(nums[2::5])
+
+# print(type(nums))
+# print(type(letters))
+
+# reverse
+# print(nums[::-1])
+# print(letters[::-1])
+
+
+mystring = 'Hello World'
+# print(mystring[-3])
+print('Hello World'[-3])
+'Hello World'[-3]
+
+print('------------------------------------------------------------------------')
+
+# x = 'fish'
+# if x == 'dog':
+#     print('it is a dog')
+# elif x == 'bird':
+#     print('it is a bird')
+# elif x == 'cat':
+#     print('it is a cat')
+# else:
+#     print('X is a: %s' % (x))
+
+# x = 'fish'
+# if x == 'dog':
+#     print('it is a dog')
+# elif x == 'bird':
+#     print('it is a bird')
+# elif x == 'cat':
+#     print('it is a cat')
+# else:
+#     print(f'X = ' % {x})
+
+
+# name = 'World'
+# program = 'Python'
+# print(f'Hello {name}! This is {program}')
+
+# name = 'World'
+# program = 'Python'
+# print(f'Hello {name}! This is {program}')
+
+# name = 'World'
+# program = 'Python'
+# print('Hello %{name}! This is %{program}')
+
+# name = 'world'
+# program = 'python'
+# print('Hello %s! This is %s.' % (name, program))
