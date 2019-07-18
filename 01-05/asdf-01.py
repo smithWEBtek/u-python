46cc822bb85c1592c0c06e8b8c1f83989475e4ee
Brad Smith <brad@smithwebtek.com>
2019-07-18 07:26:06 -0400

    Practice python iteration and format

--- 01-05/asdf-01.py
+++ 01-05/asdf-01.py
@@ -1,10 +1,10 @@
+import math
 print('hello \npython')
 # print('hello \tpython')
 # print(len('hello'))
 
-# import math
-# letters = "abcdefghijklmnopqrstuvwxyz"
-# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+letters = "abcdefghijklmnopqrstuvwxyz"
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 # slice
 # print(letters[2:5])
@@ -14,17 +14,18 @@ print('hello \npython')
 # print(letters[2:8])
 
 # step
-# print(nums[0:19:7])
-# print(nums[::])
-# print(nums[::5])
-# print(nums[2::5])
+print(nums[0:19:7])
+print(nums[::])
+print(nums[::5])
+print(nums[2::5])
 
 # print(type(nums))
 # print(type(letters))
 
-# reverse
-# print(nums[::-1])
-# print(letters[::-1])
+print('reverse nums')
+print(nums[::-1])
+print('reverse letters')
+print(letters[::-1])
 
 
 # mystring = 'Hello World'
@@ -32,15 +33,16 @@ print('hello \npython')
 # print('Hello World'[-3])
 # 'Hello World'[-3]
 
-# x = 'fish'
-# if x == 'dog':
-#     print('it is a dog')
-# elif x == 'bird':
-#     print('it is a bird')
-# elif x == 'cat':
-#     print('it is a cat')
-# else:
-#     print('X = {}'.format(x))
+print(" x is currently a fish")
+x = 'fish'
+if x == 'dog':
+    print('it is a dog')
+elif x == 'bird':
+    print('it is a bird')
+elif x == 'cat':
+    print('it is a cat')
+else:
+    print('X = {}'.format(x))
 
 # print(4 * (6 + 5))
 # print(4 * 6 + 5)
@@ -87,15 +89,12 @@ users = ({'name': 'Bob', 'age': 42, 'city': 'Boston'},
          )
 
 for u in users:
-    print(u.keys())
-    print(u.values())
-    name = u.values()[2]
-    age = u.values()[1]
-    city = u.values()[0]
-    print(name, city, age)
-    # print('{n} is from {c} and is {a} years old.'.format(n=name, c=city, a=age))
+    # print(u.keys())
+    # print(u.values())
+    u_data = list(u.values())
+    name = u_data[0]
+    age = u_data[1]
+    city = u_data[2]
+
+    print('-------------------------------------------------------')
     print('{n} is from {c} and is {a} years old.'.format(c=city, n=name, a=age))
-
-    print('------------------------------------------------------')
-
-print('-------------------------------------------------------')
